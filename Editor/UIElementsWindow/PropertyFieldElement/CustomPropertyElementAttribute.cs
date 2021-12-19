32ce0f6196a39274f69527ba2dd03598dcedb3ec
Recstazy <djexstas9@gmail.com>
2021-12-20 02:55:53 +0700

    Added attribute to use as CustomPropertyDrawer

---
+++ Editor/UIElementsWindow/PropertyFieldElement/CustomPropertyElementAttribute.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Recstazy.BehaviourTree.EditorScripts
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class CustomPropertyElementAttribute : Attribute
+    {
+        #region Fields
+	
+        #endregion
+
+        #region Properties
+	
+        #endregion
+    }
+}
