8b378ffe53158f72a7e6fc58664e43b52575441a
Recstazy <djexstas9@gmail.com>
2022-01-15 17:49:04 +0700

    Moved editor scripts to their logical folders

--- Editor/UIElementsWindow/PropertyFieldElement/CustomPropertyElementAttribute.cs
+++ Editor/UIElementsWindow/PropertyFieldElement/CustomPropertyElementAttribute.cs
@@ -5,15 +5,23 @@ using System;
 
 namespace Recstazy.BehaviourTree.EditorScripts
 {
+    /// <summary> UIElements analog of CustomPropertyDrawer </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
     public class CustomPropertyElementAttribute : Attribute
     {
         #region Fields
-	
+
         #endregion
 
         #region Properties
 	
+        public Type PropertyType { get; private set; }
+
         #endregion
+
+        public CustomPropertyElementAttribute(Type propertyType)
+        {
+            PropertyType = propertyType;
+        }
     }
 }
